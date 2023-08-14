4fec1dc3df2262f1e08ec68f4e2d533aae76f1c6
ariellopesdev <lubyel.lopes@gmail.com>
2023-08-14 17:45:51 -0300

    Change of goals

--- painel/index.php
+++ painel/index.php
@@ -1,9 +1,39 @@
-<?php
-    include('../config.php');
+<!DOCTYPE html>
+<html lang="en">
 
-    if(Painel::logado() == false){
-        include('login.php');
-    }else{
-        include('main.php');
-    }
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta name="keywords" content="Criação de campeonatos de futebol randomicos, gerenciamento, criação">
+    <meta name="description" content="Criar campeonatos">
+    <meta name="author" content="Ariel Lopes">
+    <!-- <link rel="icon" href="favicon.ico" type="image/x-icon" /> -->
+    <title>Painel de Controle</title>
+</head>
+
+<body>
+<aside class="left">
+    
+    </aside>
+    <section class="right" id="main">
+        <header>
+            <i class="fa fa-bars left" aria-hidden="true"></i>
+            <h1 class="left">Painel de Controle</h1>
+            <button class="right"><a href="#"><i class="fa fa-home" aria-hidden="true"></i>Página Inicial</a></button>
+            <button class="right"><a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i>Sair</a></button>
+        </header>
+        <main>
+            <section class="container-main">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quos deserunt aliquam tempore illum nisi ipsa corrupti voluptas cumque fuga minima porro harum qui magnam animi officia dolorum, similique facilis.</p>
+            </section>
+        </main>
+    </section>
+    <div class="clear"></div>
+</body>
+
+</html>
